ee8ee9907797a9f2186741c212e148e189d8b728
maopucheng <maopucheng@luyoo.com>
2021-06-11 14:44:18 +0800

    master

--- led-v0.1.py
+++ led-v0.1.py
@@ -1,8 +1,11 @@
 from machine import Pin
 from neopixel import NeoPixel
+import utime
 
-pin = Pin(22, Pin.OUT)
+pin = Pin(22, Pin.OUT, Pin.PULL_UP)
 np = NeoPixel(pin, 8)
-np[1] = (0, 0, 200)
-np[2] = (100, 0, 0)
-np.write()
+while True:
+    np[1] = (0, 0, 200)
+    np[2] = (100, 0, 0)
+    np.write()
+    utime.sleep_ms(300)
